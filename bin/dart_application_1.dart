7abdb2f263490ff49ec2bca1b918072dc09699e1
Samudinov <104208404+Samudinov@users.noreply.github.com>
2022-04-29 20:11:57 +0600

    init

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,43 @@
+void main(List<String> args) {
+// List list1 = [1, 7, 12, 3, 56, 2, 87, 34, 54];
+// print(list1.first);
+// print(list1[4]);
+// print(list1.last);
+
+// List list2 = [3, 12, 43, 1, 25, 6, 5, 7];
+// List list21 = [55, 11, 23, 56, 78, 1, 9];
+// list2.add(list21);
+// print(list2);
+
+List a = ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
+
+print(a.sublist(2,9));
+
+
+// List list3 = [1, 2, 3, 4, 5, 6, 7];
+//  print(list3.contains(3));
+//  print(list3.first);
+//  print(list3.last);
+//  print(list3.length);
+ 
+ 
+//  List list4 = [601, 123, 2, "dart", 45, 95, "dart24", 1];
+
+// print(list4.contains("dart"));
+// print(list4.contains(951));
+
+// List list5 = ['post', 1, 0, 'flutter'];
+// String myDart = 'Flutter';
+// print(list5.contains(myDart));
+// print(list5.length); 
+
+// List<String> list6 = ['I', 'Started', 'Learn', 'Flutter', 'Since', 'April']; 
+// String myFlutter = "";
+// myFlutter = list6.join(" * ").toString();
+// print(myFlutter);
+
+// List list7 = [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+// list7.sort ((a, b) => a.compareTo(b));
+// print(list7);
+}
+
